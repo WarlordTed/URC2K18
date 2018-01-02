c20deebeb5778e3d4e02a9f7f3e809153a613e17
deveshbajaj <deveshbajaj59@gmail.com>
2018-01-02 23:16:35 +0530

    video communcation between procees almost done

--- Video transmision/reciver.py
+++ Video transmision/reciver.py
@@ -1,8 +1,19 @@
 import cv2
+from time import sleep
+
+
+
 
 while True:
-	dev = cv2.imread("dev.bmp",1)
-	cv2.imshow('img',dev)
+	sleep(0.01)
+	dev = cv2.imread("dev.jpg",1)
+	try:
+		cv2.imshow("y",dev)
+
+	except Exception as e:
+		a=e
+
+
 	k = cv2.waitKey(60) & 0xff
 	if k == 27:
 		break
--- Video transmision/sender.py
+++ Video transmision/sender.py
@@ -1,14 +1,14 @@
-import numpy as np
-import matplotlib as plt
+from time import sleep
 import cv2
 cap = cv2.VideoCapture(0)#"192.168.43.55:5000")#'http://192.168.43.185:8080/video')
 
 while 1:
     ret ,img = cap.read()
-    print(img.shape)
-    cv2.imwrite("dev.bmp",img)
-    cv2.imshow('img',img)
-    k = cv2.waitKey(60) & 0xff
+    #print(img.shape)
+    cv2.imwrite("dev.jpg",img)
+    #cv2.imshow('img',img)
+    sleep(0.01)
+    k = cv2.waitKey(120) & 0xff
     if k == 27:
         break
 cv2.destroyAllWindows()
